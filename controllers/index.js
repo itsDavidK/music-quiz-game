584761d4eee287717da9429b27c3472738775690
jordan koichiro johnson <mrjordanjohnson@hotmail.com>
2022-11-08 10:39:12 -0800

    routes done

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-
-const homeRoutes = require('./home-routes.js');
+const apiRoutes = require('./api');
+const user = require('./user')
+const homeRoutes = require('./home-routes');
 
 router.use('/', homeRoutes);
-
+router.use('/user', user);
+router.use('/api', apiRoutes);
 
 module.exports = router;
