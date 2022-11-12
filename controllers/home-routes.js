27aeb79548ff801e768f9a7c18c8dc8879a2854d
David <davidakovalchuk@gmail.com>
2022-11-12 10:34:37 -0800

    Added gameselect page.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -169,5 +169,18 @@ router.get('/quiz-score/:id', async (req, res) => {
         console.log(err);
         res.status(500).json(err);
     }
-})
+});
+
+router.get('/gameselect', async (req, res) => {
+    try {
+        res.render('gameselect', {
+            loggedIn: req.session.loggedIn,
+            userInfo: req.session.userInfo
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
